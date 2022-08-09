20c80ccfcb4dd659cefcc4a8915717dcb0a6b4cb
Miguel Antunes <miguel.aka.kelter@gmail.com>
2022-08-09 11:44:18 +0100

    configured builds

--- rollup.tracker.min.config.js
+++ rollup.tracker.min.config.js
@@ -1,12 +1,14 @@
 import buble from '@rollup/plugin-buble';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'tracker/index.js',
   output: {
-    file: 'public/simplemetrics.js',
+    file: 'public/simplemetrics.min.js',
     format: 'iife',
   },
   plugins: [
     buble({ objectAssign: true }),
+    terser({ compress: { evaluate: false } }),
   ],
 };
